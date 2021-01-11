fd60fd1ff23d8968c6d273e79296aa72b2a11244
Jordy Chang <btbict.jordy@gmail.com>
2021-01-11 10:38:57 +0700

    commit on master

---
+++ client/src/components/Config.js
@@ -0,0 +1,15 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_KEY= '930bf5c0203d7a228d81ed707d0a4786';
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
+
+
+//Sizes: w300, w780, w1280, original
+export const BACKDROP_SIZE = 'w1280'
+export const IMAGE_SIZE = 'w1280'
+
+// w92, w154, w185, w342, w500, w780, original
+export const POSTER_SIZE = 'w500'
